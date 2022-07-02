62c735d40dabe9c3f4aa6c145d03983c8c349ce5
Farhoud <farhoud.m7@gmail.com>
2022-07-02 13:40:51 +0430

    Revert "Cluster"

--- apps/box/src/app.ts
+++ apps/box/src/app.ts
@@ -1,16 +1,18 @@
-import {createLibp2p as createNode, Libp2pOptions, Libp2p} from 'libp2p';
-import {resolveLater} from 'async-later';
+import Libp2p, {constructorOptions, Libp2pOptions} from 'libp2p';
+import * as IPFS from 'ipfs';
+import { resolveLater } from 'async-later';
 import debug from 'debug';
-import {create, IPFSHTTPClient} from "ipfs-http-client";
+import config from "config";
+import {create} from "ipfs-http-client";
 import {printBoxListeningAddrs} from "./utils";
 import {registerFile} from "./file";
-import {libConfig} from "./config";
+import {libConfig, ipfsConfig} from "./config";
 import {registerGraph, getOrbitDb} from "./graph";
-import {IPFS_CLUSTER_PROXY} from "./const";
+import {IPFS_PATH, IPFS_HTTP} from "./const";
 
 
 const [libp2pPromise, resolveLibp2p] = resolveLater<Libp2p>();
-const [ipfsPromise, resolveIpfs] = resolveLater<IPFSHTTPClient>();
+const [ipfsPromise, resolveIpfs] = resolveLater<IPFS.IPFS>();
 
 export async function getLibp2p() {
   return libp2pPromise;
@@ -20,49 +22,53 @@ export async function getIPFS() {
   return ipfsPromise;
 }
 
-
-const createLibp2p = async (conf) => {
-  try {
-    const cfg = await libConfig(conf)
-    return createNode(cfg)
-  } catch (e) {
-    console.log(e);
+async function createIPFS(createLibp2p){
+  if(IPFS_HTTP){
+    createLibp2p()
+    const libp2pNode = await getLibp2p();
+    await libp2pNode.start()
+    printBoxListeningAddrs(libp2pNode.peerId, libp2pNode.multiaddrs)
+    return  new Promise<IPFS.IPFS>(((resolve, reject) => {
+      try{
+        resolve(create({url:new URL(IPFS_HTTP)}))
+      }catch (e){
+        console.log(e)
+        reject()
+      }
+    }))
+  }else {
+    return IPFS.create({
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      libp2p: createLibp2p,
+      repo: IPFS_PATH,
+      peerId: config?.peerId,
+      config: ipfsConfig()
+    })
   }
-
 }
 
-const createIPFSClusterProxy = async () => {
-  if (!IPFS_CLUSTER_PROXY) {
-    throw new Error('No IPFS cluster provided')
-  }
-  return new Promise<IPFSHTTPClient>(((resolve, reject) => {
-    try {
-      resolve(create({url: new URL(IPFS_CLUSTER_PROXY)}))
-    } catch (e) {
-      console.log(e)
-      reject()
-    }
-  }))
-}
 
+export async function app(config?:Partial<Libp2pOptions&constructorOptions>) {
 
-export async function app(config?: Partial<Libp2pOptions>) {
-  resolveLibp2p(
-    createLibp2p(config)
-  );
+  const createLibp2p = async (config: Libp2pOptions) => {
+    resolveLibp2p(
+      Libp2p.create(await libConfig(config))
+    );
+    return libp2pPromise;
+  };
   resolveIpfs(
-    createIPFSClusterProxy()
+    createIPFS(createLibp2p)
   )
 
 
   const libp2pNode = await getLibp2p();
   const ipfsNode = await getIPFS();
-  await libp2pNode.start()
-  console.log(libp2pNode.isStarted())
-  console.log(`Box peerID: ${libp2pNode.peerId}`)
-  printBoxListeningAddrs(libp2pNode.getMultiaddrs())
-  await registerFile(libp2pNode, ipfsNode)
-  await registerGraph(libp2pNode, ipfsNode)
+  console.log('Box peerID: ' + libp2pNode.peerId.toB58String())
+
+
+  registerFile(libp2pNode, ipfsNode)
+  registerGraph(libp2pNode, ipfsNode)
   return {
     stop: async () => await graceful()
   }
@@ -70,10 +76,15 @@ export async function app(config?: Partial<Libp2pOptions>) {
 
 export async function graceful() {
   debug('\nStopping server...');
+  const ipfs = await getIPFS();
   const libp2p = await getLibp2p();
   const otbitDb = await getOrbitDb();
   await otbitDb.stop();
-  await libp2p.stop();
+  if(!IPFS_HTTP)
+    await ipfs.stop();
+  await libp2p.stop()
+  return
+  // process.exit(0);
 }
 
 
--- apps/box/tests/helper.ts
+++ apps/box/tests/helper.ts
@@ -1,29 +1,36 @@
+import Libp2p from 'libp2p';
+import WebRTCStar from 'libp2p-webrtc-star';
+import { NOISE, Noise } from '@chainsafe/libp2p-noise';
 import wrtc from 'wrtc';
-import {WebRTCStar} from '@libp2p/webrtc-star';
-import {Mplex} from '@libp2p/mplex';
-import {Noise} from "@chainsafe/libp2p-noise";
-import {createLibp2p} from "libp2p";
-import {FaultTolerance} from "libp2p/transport-manager";
-import {createEd25519PeerId } from '@libp2p/peer-id-factory'
-import {TCP} from '@libp2p/tcp';
-import {WebSockets} from '@libp2p/websockets';
-import {netSecret} from "../src/config";
-import { Multiaddr } from '@multiformats/multiaddr';
-import * as PeerId from '@libp2p/peer-id'
+import Mplex from 'libp2p-mplex';
+import {listen,netSecret} from "../src/config";
+
+new Noise();
 
 export const createClient = async () =>{
-    const node = await createLibp2p({
-        peerId: await createEd25519PeerId(),
-        connectionProtector: netSecret,
-        transportManager: {
-            faultTolerance: FaultTolerance.NO_FATAL
+    const node = await Libp2p.create({
+        addresses: {
+            listen
         },
-        connectionManager: {
-            autoDial: false
+        modules: {
+            transport: [WebRTCStar],
+            streamMuxer: [Mplex],
+            connEncryption: [NOISE],
+            connProtector:netSecret
         },
-        transports: [new WebRTCStar({wrtc}), new TCP(), new WebSockets()],
-        connectionEncryption: [new Noise()],
-        streamMuxers: [new Mplex()],
+        config: {
+            transport: {
+                [WebRTCStar.prototype[Symbol.toStringTag]]: {
+                    wrtc // You can use `wrtc` when running in Node.js
+                }
+            },
+            peerDiscovery: {
+                autoDial: false,
+                [WebRTCStar.prototype[Symbol.toStringTag]]: {
+                    enabled: false
+                }
+            }
+        }
     });
     await node.start()
     return node
@@ -31,6 +38,6 @@ export const createClient = async () =>{
 
 
 export const connect =async (clientNode, boxNode) => {
-    clientNode.peerStore.addressBook.set(boxNode.peerId, boxNode.getMultiaddrs());
+    clientNode.peerStore.addressBook.set(boxNode.peerId, boxNode.multiaddrs);
     return await clientNode.dial(await boxNode.peerId);
 }
